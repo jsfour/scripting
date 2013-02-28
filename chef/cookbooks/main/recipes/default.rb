5aa7340a6c3266252567ff8028e78c08b6aa86b2
Jimi Smoot <jsfour@gmail.com>
2013-02-27 23:22:12 -0800

    added some other features

--- chef/cookbooks/main/recipes/default.rb
+++ chef/cookbooks/main/recipes/default.rb
@@ -1,23 +1,31 @@
 # Install Packages
-execute "update package index" do
+execute "Update package index" do
   command "apt-get update"
-  ignore_failure true
-  action :nothing
-end.run_action(:run)
+  action :run
+end
 
+package "curl"
 package "git-core"
-package "tar"
-package "ntp"
-package "sysstat"
+package "openssl"
+package "build-essential"
+
 
 #include MYSQL ruby drivers
+include_recipe "mysql::server"
+include_recipe "mysql::ruby"
+include_recipe "postgresql::server"
+include_recipe "postgresql::ruby"
+
+execute "Install rvm" do
+  command "curl -L https://get.rvm.io | bash -s stable --ruby"
+  action :run
+end
 
 # Setup Environment
 execute "Configure rails development environment" do
   command "echo 'export RAILS_ENV=development' >> ~/.bashrc"
-  ignore_failure true
-  action :nothing
-end.run_action(:run)
+  action :run
+end
 
 #setup rails
 #Setup guard
