4128ce28a43fce16ca2fb9281fbf02de0e89f2b1
Jimi Smoot <jimi@pk4media.com>
2013-02-27 18:53:45 -0800

    updated

--- rails/rails_bootstrap.sh
+++ rails/rails_bootstrap.sh
@@ -2,12 +2,15 @@
 #
 #
 
+#Get essentials up and running
 sudo apt-get update
-echo "Installing CURL" | sudo apt-get install curl
-echo "Installing CURL" | sudo debconf-set-selections && sudo apt-get install chef -y
-echo "Bootstrap Complete"
+sudo apt-get install build-essential zlib1g-dev git-core sqlite3 libsqlite3-dev curl
 
 #get chef running
+curl -L https://get.rvm.io | bash -s stable --rails
+rvm install 1.9.3
+echo "Bootstrap Complete"
+
 #install Postgresql
 #install Mysql
 #install RVM
