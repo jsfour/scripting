0c890e7145303c13cf1f2a77ff0f5fa354a944b8
Jimi Smoot <jsfour@gmail.com>
2013-02-27 22:17:43 -0800

    added environment

--- chef/cookbooks/main/recipes/default.rb
+++ chef/cookbooks/main/recipes/default.rb
@@ -1,4 +1,23 @@
+# Install Packages
+execute "update package index" do
+  command "apt-get update"
+  ignore_failure true
+  action :nothing
+end.run_action(:run)
+
 package "git-core" do
   action :install
   options "--force-yes"
 end
+
+#include MYSQL ruby drivers
+
+# Setup Environment
+execute "Configure rails development environment" do
+  command "echo 'export RAILS_ENV=development' >> ~/.bashrc"
+  ignore_failure true
+  action :nothing
+end.run_action(:run)
+
+#setup rails
+#Setup guard
