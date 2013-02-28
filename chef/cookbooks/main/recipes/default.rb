b5cf639cad8efed0642505da856b0f5494b452d0
Jimi Smoot <jsfour@gmail.com>
2013-02-27 23:32:31 -0800

    added some additional

--- chef/cookbooks/main/recipes/default.rb
+++ chef/cookbooks/main/recipes/default.rb
@@ -14,6 +14,7 @@ package "build-essential"
 include_recipe "mysql::server"
 include_recipe "mysql::ruby"
 include_recipe "postgresql::server"
+include_recipe "postgresql::client"
 include_recipe "postgresql::ruby"
 
 execute "Install rvm" do
@@ -21,11 +22,18 @@ execute "Install rvm" do
   action :run
 end
 
+#setup rails
+#Setup guard
+
 # Setup Environment
+# This is not workig correctly.
 execute "Configure rails development environment" do
   command "echo 'export RAILS_ENV=development' >> ~/.bashrc"
   action :run
 end
 
-#setup rails
-#Setup guard
+execute "Configure rails development environment" do
+  command "echo 'export RAILS_ENV=development' >> ~/.bashrc"
+  action :run
+end
+
